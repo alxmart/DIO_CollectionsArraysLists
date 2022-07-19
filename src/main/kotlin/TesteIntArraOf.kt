5723eeb892e681cfaae2c807ff41da8081babe7c
alxmart <alxmartinez@gmail.com>
2022-07-19 17:50:39 -0300

    Adding TesteIntArray.kt, TesteIntArraOf.kt, TesteArrayStrings.kt

---
+++ src/main/kotlin/TesteIntArraOf.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    val values2 = intArrayOf(2, 3, 4, 1, 10, 7)
+
+    values2.forEach {
+        println(it)
+    }
+    println("")
+
+    values2.sort()
+
+    values2.forEach {
+        println(it)
+    }
+    println("")
+
+
+}
